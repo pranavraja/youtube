8e288fc03731e210dd1528dc64ff97c05a3d90e4
Pranav Raja <rickdangerous1@gmail.com>
2012-07-21 16:28:43 +1000

    changed remote mp3 provider

--- test/linkfinder_test.js
+++ test/linkfinder_test.js
@@ -4,7 +4,7 @@ var linkfinder = require('../lib/linkfinder');
 exports['get link'] = function (test) {
     linkfinder.find('Michael Buble Sway', function (error, url) {
         if (error) throw error;
-        test.equal(url.indexOf('http://www.youtube-mp3.org/get'), 0);
+		test.ok(url.match(/download/));
         test.done();
     });
 };
--- test/mp3_test.js
+++ test/mp3_test.js
@@ -5,7 +5,7 @@ exports['get link'] = function (test) {
     var videoId = 'zmYMkl1Grzc';
     youtubemp3.link(videoId, function (error, url) {
         test.ifError(error);
-        test.equal(url.indexOf('http://www.youtube-mp3.org/get?video_id=' + videoId), 0);
+		test.ok(url.match(/download/));
         test.done();
     });
 };
